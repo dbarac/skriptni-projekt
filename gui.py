ec9b8e1f0ed0a232ced6ebbc8be4045faf558f06
dbarac <dario.barac1@gmail.com>
2022-02-02 23:22:23 +0100

    add visualization change callback

--- gui.py
+++ gui.py
@@ -77,15 +77,23 @@ mainarea.pack(expand=True, fill='both', side='right')
 SALINITY = 0
 TEMPERATURE = 1
 ECOLI = 2
-def select_color_variable():
-    pass
-    #selection = "You selected the option " + str(var.get())
-    #label.config(text = selection)
 
-def select_size_variable():
-    pass
-    #selection = "You selected the option " + str(var.get())
-    #label.config(text = selection)
+size_variable = tk.IntVar()
+color_variable = tk.IntVar()
+selected_date = tk.StringVar()
+
+def update_visualization_parameters():
+    """
+    Callback function.
+
+    This will run when one of the following changes:
+     - color variable
+     - size variable
+     - the date of observations which should be visualized
+    """
+    update_visualization(
+        data, selected_date.get(), color_variable.get(), size_variable.get()
+    )
 
 def add_widgets_to_sidebar(sidebar):
     values = (SALINITY, TEMPERATURE, ECOLI)
@@ -94,65 +102,72 @@ def add_widgets_to_sidebar(sidebar):
     # create radio buttons for selecting the variable which should be used
     # to determine point size
     tk.Label(sidebar, text="Veličina kruga", font='Helvetica 11 bold').pack(fill='x')
-    size_variable = tk.IntVar()
     for val, var_name in zip(values, variable_names):
         tk.Radiobutton(
             sidebar, text=var_name, variable=size_variable, value=val,
-            command=select_size_variable, width=35
+            command=update_visualization_parameters, width=35
         ).pack(anchor=tk.W)
-    
-    separator3 = ttk.Separator(sidebar, orient='horizontal')
-    separator3.pack(fill='x')
+ 
+    ttk.Separator(sidebar, orient='horizontal').pack(fill='x')
 
     # create radio buttons for selecting the variable which should be used
     # to determine point color
     tk.Label(sidebar, text="Boja kruga", font='Helvetica 11 bold').pack(fill='x')
-    color_variable = tk.IntVar()
     for val, var_name in zip(values, variable_names):
         tk.Radiobutton(
             sidebar, text=var_name, variable=color_variable, value=val,
-            command=select_color_variable, width=35
+            command=update_visualization_parameters, width=35
         ).pack(anchor=tk.W)
 
-    separator2 = ttk.Separator(sidebar, orient='horizontal')
-    separator2.pack(fill='x')
+    ttk.Separator(sidebar, orient='horizontal').pack(fill='x')
+
+    # create dropdown menu for selecting date
     tk.Label(sidebar, text="Datum opažanja", font='Helvetica 11 bold').pack(fill='x')
-    
     dates = list(data["Datum"].unique())
-    
-    variable = tk.StringVar(sidebar)
-    variable.set(dates[0]) # default value
-    
-    w = tk.OptionMenu(sidebar, variable, *dates)
-    w.pack()
+    selected_date.set(dates[0]) # default value
+    tk.OptionMenu(sidebar, selected_date, *dates).pack()
+
+    submit_button = tk.Button(
+        sidebar, text='Update', command=update_visualization_parameters
+    )
+    submit_button.pack()
 
 add_widgets_to_sidebar(sidebar)
 
 img = mpimg.imread("kvarnerski-zaljev.png")
 fig = Figure(figsize=(5, 4), dpi=100)
 #fig.add_subplot(111).imshow(img)
-ax = fig.gca()
-ax.imshow(img)
+
 ## imgplot = plt.imshow(img)
+canvas = FigureCanvasTkAgg(fig, master=mainarea)  # A tk.DrawingArea.
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+toolbar = NavigationToolbar2Tk(canvas, mainarea)
+toolbar.update()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 def update_visualization(data, date, color_variable, size_variable):
+    fig.clf() # clear figure content
+    ax = fig.gca()
+    ax.imshow(img)
+    ax.set_title(
+        "Vizualizacija mjerenja u kvarnerskom zaljevu: "
+        "salinitet, temperatura i E. Coli"
+    )
     observations = data[data["Datum"] == date]
-    x = []
-    y = []
-    sizes = []
-    colors = []
 
-    column_names = ["Slanost", "Temperatura mora", "EC"]
-    min_values = [
+    column_names = ("Slanost", "Temperatura mora", "EC")
+    min_values = (
         observations["Slanost"].min(),
         observations["Temperatura mora"].min(),
         observations["EC"].min()
-    ]
-    max_values = [
+    )
+    max_values = (
         observations["Slanost"].max(),
         observations["Temperatura mora"].max(),
         observations["EC"].max()
-    ]
+    )
 
     def get_circle_size(size_variable, value):
         """
@@ -176,6 +191,10 @@ def update_visualization(data, date, color_variable, size_variable):
             fontsize=8, borderpad=1.5, labelspacing=2, title=column_names[size_variable]
         )
 
+    x = []
+    y = []
+    sizes = []
+    colors = []
     annotations = []
     for idx, row in observations.iterrows():
         lat, lon = beach_coords[row["Ime plaze"]]
@@ -202,54 +221,15 @@ def update_visualization(data, date, color_variable, size_variable):
     )
     fig.colorbar(points, label=column_names[color_variable])
 
+    canvas.draw()
+
 update_visualization(data, "13/05/2009", ECOLI, TEMPERATURE)
-## x = []
-## y = []
-## sizes = []
-## colors = []
-## ecoli_min = data["EC"].min()
-## ecoli_max = data["EC"].max()
-## temp_min = data["Temperatura mora"].min()
-## temp_max = data["Temperatura mora"].max()
-## salinity_min = data["Slanost"].min()
-## salinity_max = data["Slanost"].max()
-## 
-## beaches = []
-## annotations = []
-## for idx, row in data.iterrows():
-##     lat, lon = beach_coords[row["Ime plaze"]]
-##     beach_x, beach_y = map_coords_to_img_coords(img, lat, lon)
-##     if row["Ime plaze"] not in beaches:
-##         x.append(beach_x)
-##         y.append(beach_y)
-##         colors.append(row["EC"])
-##         sizes.append(200)
-##         beaches.append(row["Ime plaze"])
-##         annotations.append(
-##             row["Ime plaze"] + "\n" +
-##             "Slanost: " + str(row["Slanost"]) + "\n" +
-##             "Temperatura mora: " + str(row["Temperatura mora"]) + "\n" +
-##             "E. Coli: " + str(row["EC"])
-##         )
-## points = ax.scatter(x, y, s=sizes, c=colors, alpha=1, vmin=ecoli_min, vmax=ecoli_max)
-## mplcursors.cursor(points, hover=True).connect(
-##     "add", lambda sel: sel.annotation.set_text(annotations[sel.index])
-## )
-## fig.colorbar(points)
-#plt.show()
 
 
-canvas = FigureCanvasTkAgg(fig, master=mainarea)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-toolbar = NavigationToolbar2Tk(canvas, mainarea)
-toolbar.update()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 
 def on_key_press(event):
-    print("you pressed {}".format(event.key))
+    #print("you pressed {}".format(event.key))
     key_press_handler(event, canvas, toolbar)
 
 canvas.mpl_connect("key_press_event", on_key_press)
@@ -260,7 +240,7 @@ def _quit():
     root.destroy()  # this is necessary on Windows to prevent
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
-button = tk.Button(master=mainarea, text="Quit", command=_quit)
+button = tk.Button(master=sidebar, text="Quit", command=_quit)
 button.pack(side=tk.BOTTOM)
 
 label = tk.Label(sidebar)
@@ -268,4 +248,3 @@ label.pack()
 tk.mainloop()
 # If you put root.destroy() here, it will cause an error if the window is
 # closed with the window manager.
-
